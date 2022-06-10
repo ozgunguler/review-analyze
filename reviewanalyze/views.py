89f7e83ff86b7028315bfa77a5c3199a153d1dc6
Buğra Ahmet Çağlar <bugraahmetcaglar@gmail.com>
2022-06-10 19:04:58 +0300

    done

--- reviewanalyze/views.py
+++ reviewanalyze/views.py
@@ -14,27 +14,6 @@ from reviewanalyze.constants import BaseResponse
 from reviewanalyze.serializers import MostAverageRatingMoviesSerializer
 
 
-# class MostAverageRatingMoviesAPIView(ListAPIView):
-#     permission_classes = permissions.AllowAny,
-#     serializer_class = MostAverageRatingMoviesSerializer
-#     queryset = Ratings.objects.all()
-#
-#     @swagger_auto_schema(operation_summary="Films that have most average ratings")
-#     def list(self, request, *args, **kwargs):
-#         max_rate_films = None
-#         avg_arr = []
-#         ratings = Ratings.objects.all()
-#
-#         for i in ratings:
-#             avg_arr.append(i.averageRating)
-#         max_rate = max(avg_arr)
-#         for i in avg_arr:
-#             max_rate_films = Ratings.objects.filter(averageRating=max_rate)
-#         print(max_rate_films)
-#         context = BaseResponse.json_response(200, data="ok")
-#         return JsonResponse(200, context)
-
-
 class DeleteEmptyIDDatasDestroyAPIView(DestroyAPIView):
     permission_classes = permissions.AllowAny,
     serializer_class = MostAverageRatingMoviesSerializer
@@ -52,10 +31,8 @@ class MostAverageRatingMoviesAPIView(RetrieveAPIView):
     queryset = Ratings.objects.all()
 
     def retrieve(self, request, *args, **kwargs):
-        # ratings = Ratings.objects.all()
         movies = Movies.objects.all()
         ordered_datas = []
-        # Movies.objects.filter(titleId=ratings[i].tconst)
         avg = Ratings.objects.all().order_by('-averageRating')
         for i in avg:
             for k in movies:
@@ -67,34 +44,6 @@ class MostAverageRatingMoviesAPIView(RetrieveAPIView):
                         "numVotes": i.numVotes
                     })
         data = ordered_datas[0:20]
-        print(data)
-        # for i in range(0, 100):
-        #     x = Movies.objects.filter(titleId=ratings[i].tconst)
-        #     data.append({
-        #         "titleId": x.titleId,
-        #         "title": x.title,
-        #         "averageRating": x.averageRating,
-        #         "numVotes": x.numVotes,
-        #         "language": x.language
-        #     })
-        # max_value = max(data['averageRating'])
-        # df = pd.DataFrame(ratings)
-        # print(df)
-        # try:
-        #     rating = Ratings.objects.get(tconst=tconst)
-        # except:
-        #     context = BaseResponse.error_response(404, message="Data could not be found by given tconst (id)")
-        #     return JsonResponse(404, context)
-        # movies = Movies.objects.filter(titleId=tconst)
-        # print(movies)
-        # for i in movies:
-        #     data = {
-        #         "titleId": i.titleId,
-        #         "title": i.title,
-        #         # "averageRating": rating.averageRating,
-        #         # "numVotes": rating.numVotes,
-        #         "language": i.language
-        #     }
         context = BaseResponse.json_response(200, data)
         # context = BaseResponse.success_response(200, "ok")
         return JsonResponse(data=context, status=200)
